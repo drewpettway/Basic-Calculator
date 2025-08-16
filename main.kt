4f8f1e8e7bb08a1b7efaef5cc4861f18fa93718b
Andrew Pettway <57604557+drewpettway@users.noreply.github.com>
2025-08-16 20:40:11 +0000

    Add files via upload

--- main.kt
+++ main.kt
@@ -15,11 +15,19 @@ fun fakeClear() {
 }
 
 fun help() {
-    println("clear, version, add, subtract")
+    println("clear, version, add, subtract, multiply, divide")
 }
 
 fun version() {
-    println("CLI Calculator, 0.1")
+    println("CLI Calculator, 0.2")
+}
+
+fun multiplication(x:Int, y:Int): Int {
+    return x * y
+}
+
+fun division(x:Int, y:Int): Int {
+    return x / y
 }
 
 fun main() {
@@ -27,18 +35,17 @@ fun main() {
     while (true) { //Menu start
         println("What would you like to do? 'Type 'help' for a list of commands")
         val userCommand = readLine()
-        if (userCommand == "clear") {
-            fakeClear()
-        } else if (userCommand == "add") {
-            mode = "add"
-        } else if (userCommand == "subtract") {
-            mode = "subtract"
-        } else if (userCommand == "help") {
-            help()
-        } else if (userCommand == "version") {
-            version()
-        } else {
-            println("Invalid command")
+        when (userCommand) {
+            "clear" -> fakeClear()
+            "add" -> mode = "add"
+            "subtract" -> mode = "subtract"
+            "multiply" -> mode = "multiply"
+            "divide" -> mode = "divide"
+            "help" -> help()
+            "version" -> version()
+            "quit" -> { println("Bye"); break }
+            "debug" -> println("Mode: $mode")
+            else -> println("Invalid Command")
         } //Menu end
         if (mode == "add") { //Add block start
             println("Please type the first number")
@@ -53,7 +60,7 @@ fun main() {
                 println("Not a valid input")
             }
         } //End of add block
-        if (mode == "subtract") {
+        if (mode == "subtract") { //Subtract block start
             println("Please type the first number")
             var input1 = readLine()?.toIntOrNull()
             println("Please type the second number")
@@ -61,10 +68,42 @@ fun main() {
             if (input1 != null && input2 != null) {
                 var result = subtract(input1, input2)
                 println("The answer is $result")
+                mode = "menu"
             } else {
                 println("Not a valid input")
             }
 
-        }
-    }
+        } //End of subtract block
+
+        if (mode == "multiply") { //Multiplication block start
+            println("Please type the first number")
+            var input1 = readLine()?.toIntOrNull()
+            println("Please type the second number")
+            var input2 = readLine()?.toIntOrNull()
+            if (input1 != null && input2 != null) {
+                var result = multiplication(input1, input2)
+                println("The answer is $result")
+                mode = "menu"
+            } else {
+                println("Not a valid input")
+            }
+        } //End of multiplication block
+
+        if (mode == "divide") { //Division block start
+            println("Please type the first number")
+            var input1 = readLine()?.toIntOrNull()
+            println("Please type the second number")
+            var input2 = readLine()?.toIntOrNull()
+            if (input1 != null && input2 == 0) {
+                println("Can not divide by zero")
+            } else if (input1 != null && input2 != null) {
+                var result = division(input1, input2)
+                println("The answer is $result")
+                mode = "menu"
+            } else {
+                println("Not a valid input")
+            }
+    } //Division block end
+
+      }
 }
